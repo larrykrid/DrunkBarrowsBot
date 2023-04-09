fb55a32233e1526fe912fda9b1a142ae17661709
mlandHanze <120571801+EwwItsMike@users.noreply.github.com>
2023-04-09 16:39:57 +0200

    Refactored Finish command to Leaderboard command
    Leaderboard command now prints top 10 of the current competition. Does not clear the current competition. Mainclass now uses args to fill in token for test purposes.

--- BarrowsEventBot/src/main/java/commands/LeaderboardCommand.java
+++ BarrowsEventBot/src/main/java/commands/LeaderboardCommand.java
@@ -10,7 +10,7 @@ import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.stream.Collectors;
 
-public class FinishCommand extends Command{
+public class LeaderboardCommand extends Command{
 
     @Override
     public void execute(SlashCommandInteractionEvent event) {
@@ -18,21 +18,21 @@ public class FinishCommand extends Command{
 
         Winners winners = Competition.getInstance().getWinners();
 
-        Map<String, Integer> topFive = winners.mostItems.entrySet().stream()
+        Map<String, Integer> topTen = winners.mostItems.entrySet().stream()
                 .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
-                .limit(5)
+                .limit(10)
                 .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (e1, e2) -> e1, LinkedHashMap::new));
 
         String mostItemsList = "";
 
-        for (String key : topFive.keySet()){
-            mostItemsList += (event.getGuild().getMemberById(key).getAsMention() + " - " + topFive.get(key) + "\n");
+        for (String key : topTen.keySet()){
+            mostItemsList += (event.getGuild().getMemberById(key).getAsMention() + " - " + topTen.get(key) + "\n");
         }
 
         EmbedBuilder builder = new EmbedBuilder();
         builder.setColor(0x0000ff);
         builder.setThumbnail("https://cdn.discordapp.com/attachments/585386371968139276/1090335893908377690/drinks.png");
-        builder.setTitle("Drunk Barrows Scoreboard");
+        builder.setTitle("Drunk Barrows Leaderboard");
         builder.addField("__Most items received:__", mostItemsList, true);
 
         if (Competition.getInstance().hasItemOfTheDay()){
@@ -49,9 +49,6 @@ public class FinishCommand extends Command{
             builder.addField("__Most Brother of the Day items received:__", IOTDscoreboard, true);
         }
         builder.setFooter("Thank you for participating! See you double next time!");
-
-        Competition.getInstance().printEventSummary(event);
-//        Competition.getInstance().resetCompetition();
 
         event.getHook().sendMessageEmbeds(builder.build()).queue();
     }
